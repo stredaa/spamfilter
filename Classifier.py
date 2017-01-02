98c43a78a00349eae042dece6cdcec3a019e3ed5
stredaa <stredajr@evron.cz>
2017-01-02 20:08:24 +0100

    Classifier, Parser separation; bug fixes
    Parser can handle both email sources and exported emails, Jupyter notebook now contains tools to evaluate the classifier on given datasets/

---
+++ Classifier.py
@@ -0,0 +1,31 @@
+import numpy
+import math
+from cvxpy import *
+
+class Classifier:
+    a = None
+    b = None
+
+    #CVXPY convex optimization solver, given vectorized emailes it tries to find optimal parameter values for logistic regression curve.
+    @staticmethod
+    def getModelParams(data, labels, size):
+        a = Variable(size, 1)
+        b = Variable()
+        spam = sum(labels)
+        testing = numpy.matrix(labels)
+
+        logLogistic = sum([logistic(data[i] * a + b) for i in xrange(len(data))])
+        positive = testing * data * a  + b * spam
+        problem = Problem(Maximize(positive - logLogistic))
+
+        result = problem.solve()
+        return a.value, b.value
+
+    #Classify sample
+    def evaluate(self, sample):
+        if self.a is None or self.b is None:
+            raise ValueError("Model parameters not set!")
+        return math.e**(sample * self.a + self.b)[0, 0] / (1 + math.e**(sample * self.a + self.b)[0, 0])
+
+    def __init__(self, data, labels):
+        self.a, self.b = Classifier.getModelParams(numpy.matrix(data), labels, len(data[0]))
---
+++ Parser.py
@@ -0,0 +1,45 @@
+import email
+import re
+
+class Parser:
+    dictionary = None
+
+    #stripHeaders helper conjoining multiple substitutions into one regex
+    @staticmethod
+    def multiRegexSubstitute(regexArray, text, substitute = ""):
+        rules = "|".join(regexArray)
+        pattern = re.compile(rules)
+        return pattern.sub(substitute, text)
+
+    #Parse the standard email structure into a plaintext (strip headers, footers and section separators)
+    @staticmethod
+    def stripHeaders(mail):
+        text = ""
+        for msg in email.message_from_string(mail).walk():
+            try:
+                text += msg.get_payload() + " "
+            except:
+                text += mail
+        if "delivered-to" in text:
+            text = text[re.search("\n\n", text).start()+1:]
+
+        #REGEX punctuation mark removal
+        text = Parser.multiRegexSubstitute(["\.", "\?", ";", ",", "\"", "'", "=", "#", "[0-9]", "\*", "!", "%"], text) # non-expanding substitutions
+        text = Parser.multiRegexSubstitute(["-", "\(", "\)", "\n", "\t", "&nbsp", "_", "&", "$", "@", ":", "\[", "\]"], text, " ") # expanding substitutions
+
+        text = re.sub("<[^<>]*>", " ", text) #remove HTML/XML tags
+        text = re.sub(" +" ," ", text) #finisher whitespace removal
+
+        return text
+
+    def __init__(self, dictionary):
+        self.dictionary = dictionary
+
+    #Translate a given email according into a word vector according to the preset dictionary
+    def parseEmail(self, mail):
+        parsedData = [0] * len(self.dictionary)
+        plaintext = Parser.stripHeaders(mail).split(" ")
+        for x in plaintext:
+            if x in self.dictionary:
+                parsedData[self.dictionary.index(x)] += 1
+        return parsedData
